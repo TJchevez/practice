6119ad5e2411505c7f6d4bed7511a0f2dd99e871
Thanya Chevez <cheveztj@gmail.com>
2025-07-02 09:18:57 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,6 +2,5 @@ const palindrome = (str) => {
 const middle = Math.floor (str.length/2);
 for (let i=0; i<middle; ++i {
 const oppositeIndex = str.length -1 -index;
-if (str[i] !== str[oppositeIndex]) {
-    
+if (str[i] !== str[op
 };
