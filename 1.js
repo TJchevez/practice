c48f78e916513cc100d6550e645f2e273c074da8
Thanya Chevez <cheveztj@gmail.com>
2025-07-29 21:48:45 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,8 +1,11 @@
-const arrayChunk = (array, size) => {
+const arrayChunk2 = (array, size) => {
   const chunkedArray = [];
 
 
-  for (let i = 0; i < array.length; i += size) {
+  for (let i = 0; i < array.length; ++i) {
+    if (i % size !== 0) {
+      continue;
+    }
     const subarray = array.slice(i, i + size);
     chunkedArray.push(subarray);
   }
@@ -10,3 +13,4 @@ const arrayChunk = (array, size) => {
 
   return chunkedArray;
 };
+
