95a9198952a4507e4439e6c0dbc41b41282143da
Thanya Chevez <cheveztj@gmail.com>
2025-07-29 21:47:19 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,19 +1,9 @@
- const maxCharacter = (str) => {
-   const charMap = new Map();
-   let max = 0;
-   let maxChar = "";
-
-
-   for (const char of str) {
-     charMap.set(char, charMap.get(char) + 1 || 1)
-   }
-
-
-   for (const [char, count] of charMap) {
-     if (count > max) {
-       max = count;
-       maxChar = char;
-     }
-   }
-   return maxChar;
+const anagrams = (strA, strB) => {
+  return cleanString(strA) === cleanString(strB);
 };
+
+
+const cleanString = (str) => {
+  return str.replaceAll(" ", "").toLowerCase().split("").sort().join("");
+};
+
