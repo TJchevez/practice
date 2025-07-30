41f2cc3225f58c6559cac7521ad055973307b49d
Thanya Chevez <cheveztj@gmail.com>
2025-07-29 21:46:43 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,5 +1,26 @@
-const palindrome = (str) => {
-  return str.split("").every((char, index) => (
-    char === str[str.length - 1 - index]
-  ))
+const maxCharacter = (str) => {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char] = charMap[char] + 1;
+    }
+    else {
+      charMap[char] = 1;
+    }
+  }
+
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+
+  return maxChar;
 };
