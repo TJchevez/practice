7946a3fd5a397f174f97ad68374a1a9207481d0f
Thanya Chevez <cheveztj@gmail.com>
2025-08-02 14:50:49 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,9 +1,5 @@
-const stringifyId = (arr) => {
-  const idArray = arr.map(obj => obj.id);
-
-
-  const uniqueIdArray = [...new Set(idArray)];
-
-
-  return uniqueIdArray.join(", ");
+const reverseString = (str) => {
+  return str
+    .split("")
+    .reduce((accumulator, currentValue) => currentValue + accumulator);
 };
