60791b637bd8ce182df7c0c032372a546d60dae7
Thanya Chevez <cheveztj@gmail.com>
2025-08-05 19:50:47 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,7 +1,5 @@
-function midpoint() {
-  return (
-    (Math.min.apply(null, arguments) + Math.max.apply(null, arguments)) / 2
-  );
+function myFunction(x, y) {
+  if (y === undefined) {
+    y = 2;
+  }
 }
-
-console.log(midpoint(3, 1, 4, 1, 5)); // 3
