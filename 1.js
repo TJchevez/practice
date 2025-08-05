4dfa61e5acaa50d1301e7ed11f85fba14bea91a7
Thanya Chevez <cheveztj@gmail.com>
2025-08-05 19:50:13 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,18 +1,7 @@
-function funcWithDefault(a = 55) {
-  arguments[0] = 99; // updating arguments[0] does not also update a
-  console.log(a);
+function midpoint() {
+  return (
+    (Math.min.apply(null, arguments) + Math.max.apply(null, arguments)) / 2
+  );
 }
-funcWithDefault(10); // 10
 
-function funcWithDefault2(a = 55) {
-  a = 99; // updating a does not also update arguments[0]
-  console.log(arguments[0]);
-}
-funcWithDefault2(10); // 10
-
-// An untracked default parameter
-function funcWithDefault3(a = 55) {
-  console.log(arguments[0]);
-  console.log(arguments.length);
-}
-funcWithDefault3(); // undefined; 0
+console.log(midpoint(3, 1, 4, 1, 5)); // 3
