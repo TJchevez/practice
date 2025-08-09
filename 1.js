4e3afc4d84109205135fe845ed45899188e4f4a5
Thanya Chevez <cheveztj@gmail.com>
2025-08-09 08:47:03 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,7 +2,7 @@ const maxCharacter = (str) => {
   const charMap = {};
   let max = 0;
   let maxChar = "";
-  
+
   
   for (let char of str) {
     if (charMap[char]) {
@@ -12,7 +12,6 @@ const maxCharacter = (str) => {
       charMap[char] = 1;
     }
   }
-
 
   for (let char in charMap) {
     if (charMap[char] > max) {
