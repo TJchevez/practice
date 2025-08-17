9d65d3786eca26dde106e74a7cc36e3fc868d7ac
Thanya Chevez <cheveztj@gmail.com>
2025-08-17 15:05:26 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,39 +1,10 @@
- const anagrams1 = (strA, strB) => {
-  const charMapA = buildCharMap(strA);
-  const charMapB = buildCharMap(strB);
-
-
-  if (charMapA.size !== charMapB.size) {
-    return false;
-  }
-
-
-  for (const [charA, countA] of charMapA) {
-    if (charMapB.get(charA) !== countA) {
-      return false;
-    }
-  }
-
-
-  return true;
+const anagrams = (strA, strB) => {
+  return cleanString(strA) === cleanString(strB);
 };
 
 
-const buildCharMap = (str) => {
-  const charMap = new Map();
-
-
-  for (const char of removeSpacesAndLowerCase(str)) {
-    charMap.set(char, charMap.get(char) + 1 || 1);
-  }
-
-
-  return charMap;
-}
-
-
-const removeSpacesAndLowerCase = (str) => {
-  return str.toLowerCase().replaceAll(" ", "")
-}
+const cleanString = (str) => {
+  return str.replaceAll(" ", "").toLowerCase().split("").sort().join("");
+};
 
 
