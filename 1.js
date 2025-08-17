d73a1a7f6ba274e1d9e566e696d8a7c7978956ba
Thanya Chevez <cheveztj@gmail.com>
2025-08-17 15:03:28 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,15 +1,8 @@
-const steps2 = (n) => {
-  const spacesArray = new Array(n - 1).fill(" ");
-
-
-  for (let row = 0; row < n; ++row) {
-    const hashArray = new Array(row + 1).fill("#");
-
-
-    const step = [...hashArray, ...spacesArray.slice(0, n - hashArray.length)];
-
-
-    console.log(step.join(""));
+const capitalize = (str) => {
+  const result = [];
+  for (let word of str.split(" ")) {
+    const capitalizedWord = word[0].toUpperCase() + word.slice(1);
+    result.push(capitalizedWord);
   }
+  return result.join(" ");
 };
-
