1a4bdec05c83a45550234400893a4d43b15733f5
Thanya Chevez <cheveztj@gmail.com>
2025-08-18 18:19:30 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,6 +2,8 @@ var createCounter = function(n) {
     
     return function() {
         return n++;
-        
+        console.log(counter()); 
+        console.log(counter());
+        console.log(counter());
     };
 };
