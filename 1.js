bc7183a2b86797ab83030c8136be97475f6a27b4
Thanya Chevez <cheveztj@gmail.com>
2025-08-21 21:19:29 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,11 +1,20 @@
-var createCounter = function(n) {
-    
-    return function() {
-        return n++;
-        console.log(counter()); 
-        console.log(counter());
-        console.log(counter());
-    };
+ const pyramid = (n) => {
+  const columnWidth = 2 * n - 1;
+  const middle = Math.floor(columnWidth / 2);
+
+
+  for (let row = 0; row < n; ++row) {
+    let step = "";
+
+
+    for (let column = 0; column < columnWidth; ++column) {
+      if (column >= middle - row && column <= middle + row) {
+        step += "#"
+      }
+      else {
+        step += " "
+      }
+    }
+    console.log(step)
+  }
 };
-
-
