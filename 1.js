b60c01e36898696279ad7ac9df8039f01c92f0e0
Thanya Chevez <cheveztj@gmail.com>
2025-08-22 21:17:50 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,4 +1,5 @@
 const palindrome = (str) => {
-  const reversedString = str.split("").reverse().join("");
-  return str === reversedString;
+  return str.split("").every((char, index) => (
+    char === str[str.length - 1 - index]
+  ))
 };
