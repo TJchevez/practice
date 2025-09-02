fb111c7f32a43d538e68f42bf84aa37d0833de9f
Thanya Chevez <cheveztj@gmail.com>
2025-09-01 22:08:16 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,24 +1,9 @@
-const stringifyId2 = (arr) => {
+const stringifyId = (arr) => {
   const idArray = arr.map(obj => obj.id);
-  const uniqueArray = [];
-  let idString = "";
 
 
-  for (let id of idArray) {
-    if (!uniqueArray.includes(id)) {
-      uniqueArray.push(id);
-    }
-  }
+  const uniqueIdArray = [...new Set(idArray)];
 
 
-  for (let i = 0; i < uniqueArray.length; ++i) {
-    if (i !== uniqueArray.length - 1) {
-      idString += `${uniqueArray[i]}, `;
-    }
-    else {
-      idString += uniqueArray[i];
-    }
-  }
-
-
-  return idString;
+  return uniqueIdArray.join(", ");
+};
