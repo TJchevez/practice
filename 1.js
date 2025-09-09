72cd80dc81be502d817600a9feb059350e59657e
Thanya Chevez <cheveztj@gmail.com>
2025-09-09 13:32:55 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,24 +1,6 @@
-const stringifyId2 = (arr) => {
-  const idArray = arr.map(obj => obj.id);
-  const uniqueArray = [];
-  let idString = "";
+const reverseInteger = (int) => {
+  const reversed = int.toString().split("").reverse().join("");
 
 
-  for (let id of idArray) {
-    if (!uniqueArray.includes(id)) {
-      uniqueArray.push(id);
-    }
-  }
-
-
-  for (let i = 0; i < uniqueArray.length; ++i) {
-    if (i !== uniqueArray.length - 1) {
-      idString += `${uniqueArray[i]}, `;
-    }
-    else {
-      idString += uniqueArray[i];
-    }
-  }
-
-
-  return idString;
+  return parseInt(reversed) * Math.sign(int);
+};
