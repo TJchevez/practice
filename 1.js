241e4565d9e360fcbd941d357c2a9f28f3a156ff
Thanya Chevez <cheveztj@gmail.com>
2025-09-09 13:32:23 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,26 +1,19 @@
-const maxCharacter = (str) => {
-  const charMap = {};
-  let max = 0;
-  let maxChar = "";
+ const maxCharacter = (str) => {
+   const charMap = new Map();
+   let max = 0;
+   let maxChar = "";
 
 
-  for (let char of str) {
-    if (charMap[char]) {
-      charMap[char] = charMap[char] + 1;
-    }
-    else {
-      charMap[char] = 1;
-    }
-  }
+   for (const char of str) {
+     charMap.set(char, charMap.get(char) + 1 || 1)
+   }
 
 
-  for (let char in charMap) {
-    if (charMap[char] > max) {
-      max = charMap[char];
-      maxChar = char;
-    }
-  }
-
-
-  return maxChar;
+   for (const [char, count] of charMap) {
+     if (count > max) {
+       max = count;
+       maxChar = char;
+     }
+   }
+   return maxChar;
 };
