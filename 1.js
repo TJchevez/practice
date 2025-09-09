e5be8e69daad72eb0c699296ee999e504d5f6330
Thanya Chevez <cheveztj@gmail.com>
2025-09-09 13:31:21 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,13 +1,6 @@
-const steps = (n) => {
-  for (let row = 0; row < n; ++row) {
-    let step = "";
-    for (let column = 0; column < n; ++column) {
-      if (column <= row) {
-        step += "#";
-      } else {
-        step += " ";
-      }
-    }
-    console.log(step);
-  }
+const reverseInteger = (int) => {
+  const reversed = int.toString().split("").reverse().join("");
+
+
+  return parseInt(reversed) * Math.sign(int);
 };
